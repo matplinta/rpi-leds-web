49dab3ae997a0d196551e30ff175b505617c3462
Mateusz Plinta <matplinta@gmail.com>
2020-11-11 20:14:42 +0100

    add install; refactor leds code; add hex support

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Raspberry Pi Leds Setup
 ## Cli
 ### Usage
-![img](resources/usage.png)
+
 
 Running application with no parameters results in sending an X11 window from the remote host (provided that ssh was run with an `-X` option), which allows for selecting the preferable colour:
 
---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+    echo "Not running as root"
+    exit
+fi
+
+cp ./leds /usr/local/bin/leds
+chmod +x /usr/local/bin/leds
+
+rsync -R ./resources/colours.conf /home/pi/.config/leds/colours.conf 
+
+cp ./resources/etc/bash_completion.d/leds /etc/bash_completion.d/leds
